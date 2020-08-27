a7b564b8bf8b3330f52bd4164be1438c84aae105
Yum Cha <37918930+JackYumCha@users.noreply.github.com>
2020-08-27 13:40:17 +1000

    Update aws-use.cmd

--- aws-use.cmd
+++ aws-use.cmd
@@ -7,6 +7,6 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`call aws configure get aws_access_key_id --p
 ECHO AWS_ACCESS_KEY_ID=%AWS_ACCESS_KEY_ID%
 FOR /F "tokens=* USEBACKQ" %%F IN (`call aws configure get aws_secret_access_key --profile %profile%`) DO ( SET AWS_SECRET_ACCESS_KEY=%%F )
 ECHO AWS_SECRET_ACCESS_KEY=%AWS_SECRET_ACCESS_KEY%
-FOR /F "tokens=* USEBACKQ" %%i IN (`call aws sts get-caller-identity --profile %profile% --output text`) DO ( SET line=%%i )
+FOR /F "tokens=* USEBACKQ" %%i IN (`call aws sts get-caller-identity --profile %profile% --region %AWS_DEFAULT_REGION% --output text`) DO ( SET line=%%i )
 SET AWS_ACCOUNT_ID=%line:~0,12%
 ECHO AWS_ACCOUNT_ID=%AWS_ACCOUNT_ID%
